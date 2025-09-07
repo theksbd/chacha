dc349c77d8c1cad091c718a6b0fc871f3cf2bb3c
Do Thien Hoang <theksbd@gmail.com>
2025-09-07 23:21:24 +0700

    change image path

--- src/generateMedia.js
+++ src/generateMedia.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const mediaDir = path.join(__dirname, '../media');
+const mediaDir = path.join(__dirname, '../public');
 const outputFile = path.join(__dirname, '../src/media.js');
 
 // Allowed extensions
--- src/media.js
+++ src/media.js
@@ -2,566 +2,566 @@
 export const media = [
   {
     type: 'image',
-    src: '../media/01c7363a84e70fb956f6.jpg'
+    src: '/01c7363a84e70fb956f6.jpg'
   },
   {
     type: 'image',
-    src: '../media/0435761dc5c04e9e17d1.jpg'
+    src: '/0435761dc5c04e9e17d1.jpg'
   },
   {
     type: 'image',
-    src: '../media/075cc6df2302a85cf113.jpg'
+    src: '/075cc6df2302a85cf113.jpg'
   },
   {
     type: 'image',
-    src: '../media/0916fbc3171e9c40c50f.jpg'
+    src: '/0916fbc3171e9c40c50f.jpg'
   },
   {
     type: 'image',
-    src: '../media/0a433df2d12f5a71033e.jpg'
+    src: '/0a433df2d12f5a71033e.jpg'
   },
   {
     type: 'image',
-    src: '../media/0ad82f4fc1924acc1383.jpg'
+    src: '/0ad82f4fc1924acc1383.jpg'
   },
   {
     type: 'image',
-    src: '../media/0c0f427df2a079fe20b1.jpg'
+    src: '/0c0f427df2a079fe20b1.jpg'
   },
   {
     type: 'image',
-    src: '../media/0c4233eddd30566e0f21.jpg'
+    src: '/0c4233eddd30566e0f21.jpg'
   },
   {
     type: 'image',
-    src: '../media/0eee0632b6ef3db164fe.jpg'
+    src: '/0eee0632b6ef3db164fe.jpg'
   },
   {
     type: 'image',
-    src: '../media/10dc0df4be2935776c38.jpg'
+    src: '/10dc0df4be2935776c38.jpg'
   },
   {
     type: 'image',
-    src: '../media/156539d4d5095e570718.jpg'
-  },
-  {
-    type: 'video',
-    src: '../media/1728490257768848851.mp4'
-  },
-  {
-    type: 'image',
-    src: '../media/18c3308a835708095146.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/1b11c3f97024fb7aa235.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/1dba5711b9cc32926bdd.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/20250614_105615_1.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/20250614_105622_1.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/20250614_105638_1.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/20250614_140452_1.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/20250907_094315.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/270cced52c08a756fe19.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/2b7376c89a15114b4804.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/2cd88c4b6296e9c8b087.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/2e2ebd9c0d41861fdf50.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/2e851d3df3e078be21f1.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/30ea8a3139ecb2b2ebfd.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/3202be5c5e81d5df8c90.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/34582ab4986913374a78.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/352ed9a36b7ee020b96f.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/36f71852fa8f71d1289e.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/3b35259ec943421d1b52.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/3b9fb5ae06738d2dd462.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/3e82446fa8b223ec7aa3.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/40a1c47377aefcf0a5bf.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/4181e7f10a2c8172d83d.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/424e967b75a6fef8a7b7.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/4254f09c1241991fc050.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/4558f05c10819bdfc290.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/4567131af3c7789921d6.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/4e63dda36d7ee620bf6f.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/4fbf04b6e16b6a35337a.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/542dafb54a68c1369879.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/5430c36773baf8e4a1ab.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/56dcc582765ffd01a44e.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/58d96dacdd71562f0f60.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/59133c298ff404aa5de5.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/595720c0c51d4e43170c.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/597678729baf10f149be.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/5bb73db18e6c05325c7d.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/5cf81c8ef1537a0d2342.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/5e59a56216bf9de1c4ae.jpg'
+    src: '/156539d4d5095e570718.jpg'
   },
   {
     type: 'video',
-    src: '../media/6050363047486569317.mp4'
+    src: '/1728490257768848851.mp4'
   },
   {
     type: 'image',
-    src: '../media/6569d087605aeb04b24b.jpg'
+    src: '/18c3308a835708095146.jpg'
   },
   {
     type: 'image',
-    src: '../media/65c73ba1d67c5d22046d.jpg'
+    src: '/1b11c3f97024fb7aa235.jpg'
   },
   {
     type: 'image',
-    src: '../media/677d92ae6273e92db062.jpg'
+    src: '/1dba5711b9cc32926bdd.jpg'
   },
   {
     type: 'image',
-    src: '../media/68d8d2ea3237b969e026.jpg'
+    src: '/20250614_105615_1.jpg'
   },
   {
     type: 'image',
-    src: '../media/6974421cf1c17a9f23d0.jpg'
+    src: '/20250614_105622_1.jpg'
   },
   {
     type: 'image',
-    src: '../media/6c8abfbf0c62873cde73.jpg'
+    src: '/20250614_105638_1.jpg'
   },
   {
     type: 'image',
-    src: '../media/6f0bb082535fd801814e.jpg'
+    src: '/20250614_140452_1.jpg'
   },
   {
     type: 'image',
-    src: '../media/720da3c04d1dc6439f0c.jpg'
+    src: '/20250907_094315.jpg'
   },
   {
     type: 'image',
-    src: '../media/7392e4da5707dc598516.jpg'
+    src: '/270cced52c08a756fe19.jpg'
   },
   {
     type: 'image',
-    src: '../media/779a1bcfa812234c7a03.jpg'
+    src: '/2b7376c89a15114b4804.jpg'
   },
   {
     type: 'image',
-    src: '../media/787762b6806b0b35527a.jpg'
+    src: '/2cd88c4b6296e9c8b087.jpg'
   },
   {
     type: 'image',
-    src: '../media/7b210ae7b93a32646b2b.jpg'
+    src: '/2e2ebd9c0d41861fdf50.jpg'
   },
   {
     type: 'image',
-    src: '../media/7bc63b678bba00e459ab.jpg'
+    src: '/2e851d3df3e078be21f1.jpg'
   },
   {
     type: 'image',
-    src: '../media/7cd6b8ce0b13804dd902.jpg'
+    src: '/30ea8a3139ecb2b2ebfd.jpg'
   },
   {
     type: 'image',
-    src: '../media/7d06bf1b5cc6d7988ed7.jpg'
+    src: '/3202be5c5e81d5df8c90.jpg'
   },
   {
     type: 'image',
-    src: '../media/815843b3b36e3830617f.jpg'
+    src: '/34582ab4986913374a78.jpg'
   },
   {
     type: 'image',
-    src: '../media/8235dcb93e64b53aec75.jpg'
+    src: '/352ed9a36b7ee020b96f.jpg'
+  },
+  {
+    type: 'image',
+    src: '/36f71852fa8f71d1289e.jpg'
+  },
+  {
+    type: 'image',
+    src: '/3b35259ec943421d1b52.jpg'
+  },
+  {
+    type: 'image',
+    src: '/3b9fb5ae06738d2dd462.jpg'
+  },
+  {
+    type: 'image',
+    src: '/3e82446fa8b223ec7aa3.jpg'
+  },
+  {
+    type: 'image',
+    src: '/40a1c47377aefcf0a5bf.jpg'
+  },
+  {
+    type: 'image',
+    src: '/4181e7f10a2c8172d83d.jpg'
+  },
+  {
+    type: 'image',
+    src: '/424e967b75a6fef8a7b7.jpg'
+  },
+  {
+    type: 'image',
+    src: '/4254f09c1241991fc050.jpg'
+  },
+  {
+    type: 'image',
+    src: '/4558f05c10819bdfc290.jpg'
+  },
+  {
+    type: 'image',
+    src: '/4567131af3c7789921d6.jpg'
+  },
+  {
+    type: 'image',
+    src: '/4e63dda36d7ee620bf6f.jpg'
+  },
+  {
+    type: 'image',
+    src: '/4fbf04b6e16b6a35337a.jpg'
+  },
+  {
+    type: 'image',
+    src: '/542dafb54a68c1369879.jpg'
+  },
+  {
+    type: 'image',
+    src: '/5430c36773baf8e4a1ab.jpg'
+  },
+  {
+    type: 'image',
+    src: '/56dcc582765ffd01a44e.jpg'
+  },
+  {
+    type: 'image',
+    src: '/58d96dacdd71562f0f60.jpg'
+  },
+  {
+    type: 'image',
+    src: '/59133c298ff404aa5de5.jpg'
+  },
+  {
+    type: 'image',
+    src: '/595720c0c51d4e43170c.jpg'
+  },
+  {
+    type: 'image',
+    src: '/597678729baf10f149be.jpg'
+  },
+  {
+    type: 'image',
+    src: '/5bb73db18e6c05325c7d.jpg'
+  },
+  {
+    type: 'image',
+    src: '/5cf81c8ef1537a0d2342.jpg'
+  },
+  {
+    type: 'image',
+    src: '/5e59a56216bf9de1c4ae.jpg'
   },
   {
     type: 'video',
-    src: '../media/8245780632365356182.mp4'
+    src: '/6050363047486569317.mp4'
   },
   {
     type: 'image',
-    src: '../media/84e5a3f7432ac874913b.jpg'
+    src: '/6569d087605aeb04b24b.jpg'
   },
   {
     type: 'image',
-    src: '../media/8659dc933f4eb410ed5f.jpg'
+    src: '/65c73ba1d67c5d22046d.jpg'
   },
   {
     type: 'image',
-    src: '../media/86aec87038adb3f3eabc.jpg'
+    src: '/677d92ae6273e92db062.jpg'
   },
   {
     type: 'image',
-    src: '../media/86b1b9dc0a01815fd810.jpg'
+    src: '/68d8d2ea3237b969e026.jpg'
   },
   {
     type: 'image',
-    src: '../media/86b2e35e138398ddc192.jpg'
+    src: '/6974421cf1c17a9f23d0.jpg'
   },
   {
     type: 'image',
-    src: '../media/8784af5d4f80c4de9d91.jpg'
-  },
-  {
-    type: 'video',
-    src: '../media/8881480705327649134.mp4'
+    src: '/6c8abfbf0c62873cde73.jpg'
   },
   {
     type: 'image',
-    src: '../media/8f0557a1b47c3f22666d.jpg'
+    src: '/6f0bb082535fd801814e.jpg'
   },
   {
     type: 'image',
-    src: '../media/91be7f769cab17f54eba.jpg'
+    src: '/720da3c04d1dc6439f0c.jpg'
   },
   {
     type: 'image',
-    src: '../media/940f071ce5c16e9f37d0.jpg'
+    src: '/7392e4da5707dc598516.jpg'
   },
   {
     type: 'image',
-    src: '../media/992a40bdae60253e7c71.jpg'
+    src: '/779a1bcfa812234c7a03.jpg'
   },
   {
     type: 'image',
-    src: '../media/99e590ed2230a96ef021.jpg'
+    src: '/787762b6806b0b35527a.jpg'
   },
   {
     type: 'image',
-    src: '../media/9a12c5de7703fc5da512.jpg'
+    src: '/7b210ae7b93a32646b2b.jpg'
   },
   {
     type: 'image',
-    src: '../media/9aa3741298cf13914ade.jpg'
+    src: '/7bc63b678bba00e459ab.jpg'
   },
   {
     type: 'image',
-    src: '../media/a2556ae9d934526a0b25.jpg'
+    src: '/7cd6b8ce0b13804dd902.jpg'
   },
   {
     type: 'image',
-    src: '../media/a326e7ee07338c6dd522.jpg'
+    src: '/7d06bf1b5cc6d7988ed7.jpg'
   },
   {
     type: 'image',
-    src: '../media/a852c7b82765ac3bf574.jpg'
+    src: '/815843b3b36e3830617f.jpg'
   },
   {
     type: 'image',
-    src: '../media/a9ee38168bcb009559da.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/aef17237c0ea4bb412fb.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/aefd9a4a7497ffc9a686.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/b30038d5d4085f560619.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/b4410ea5bc7837266e69.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/b5a6fe0d4dd0c68e9fc1.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/b5afd94e3993b2cdeb82.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/bb28dc0e3cd3b78deec2.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/bcf584f86425ef7bb634.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/bd534697f44a7f14265b.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/be001dd6ad0b26557f1a.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/c006a31240cfcb9192de.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/c3a16a7d9aa011fe48b1.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/c4afd7b3646eef30b67f.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/ca44a8221bff90a1c9ee.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/cc6d2edfcb02405c1913.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/cfdc6f4edc9357cd0e82.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/d344c3bb2366a838f177.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/d36dee385de5d6bb8ff4.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/d42f14dda7002c5e7511.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/d50f8ad57a08f156a819.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/d60555e0e53d6e63372c.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/d7bb515ea1832add7392.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/d9a1dc663ebbb5e5ecaa.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/d9aa7fc29f1f14414d0e.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/d9d3720582d8098650c9.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/db89fca64f7bc4259d6a.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/dc87315c838108df5190.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/dd698ea36b7ee020b96f.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/ddafcd787ea5f5fbacb4.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/e0bddf5b2f86a4d8fd97.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/e23b4513f7ce7c9025df.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/edc2b60e06d38d8dd4c2.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/edf13f48da9551cb0884.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/ee8aa32e4df3c6ad9fe2.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/ef3630de8203095d5012.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/ef9ce4ea07378c69d526.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/f0ef353f85e20ebc57f3.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/f0efe2a0507ddb23826c.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/f37151e4e33968673128.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/f7fdbb980845831bda54.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/f84a0042b39f38c1618e.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/ffd104a6b77b3c25656a.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/IMG_20250618_083707.jpg'
-  },
-  {
-    type: 'image',
-    src: '../media/Messenger_creation_1535E073-CBFF-4944-8E22-12593BB730F0.jpeg'
-  },
-  {
-    type: 'image',
-    src: '../media/Messenger_creation_18E7AAF5-A9C5-4E4D-81B5-65E06746B19A.jpeg'
+    src: '/8235dcb93e64b53aec75.jpg'
   },
   {
     type: 'video',
-    src: '../media/Messenger_creation_1F24CF12-9F3B-492C-ACDD-9629324A441D_1.mp4'
+    src: '/8245780632365356182.mp4'
   },
   {
     type: 'image',
-    src: '../media/Messenger_creation_2B7ED779-8884-491C-B233-33947A2BA399.jpeg'
+    src: '/84e5a3f7432ac874913b.jpg'
   },
   {
     type: 'image',
-    src: '../media/Messenger_creation_40D80B68-2B2D-4101-B440-052BC955AED9.jpeg'
+    src: '/8659dc933f4eb410ed5f.jpg'
   },
   {
     type: 'image',
-    src: '../media/Messenger_creation_60C2D54F-D6A8-4638-887C-69BAED18AD35_1.jpeg'
+    src: '/86aec87038adb3f3eabc.jpg'
   },
   {
     type: 'image',
-    src: '../media/Messenger_creation_6F3ED426-7196-44F7-9C76-0D1A1DB764BF_1.jpeg'
+    src: '/86b1b9dc0a01815fd810.jpg'
+  },
+  {
+    type: 'image',
+    src: '/86b2e35e138398ddc192.jpg'
+  },
+  {
+    type: 'image',
+    src: '/8784af5d4f80c4de9d91.jpg'
   },
   {
     type: 'video',
-    src: '../media/Messenger_creation_B88301E6-8F0E-46DD-9B47-B1107D53084A.mp4'
+    src: '/8881480705327649134.mp4'
+  },
+  {
+    type: 'image',
+    src: '/8f0557a1b47c3f22666d.jpg'
+  },
+  {
+    type: 'image',
+    src: '/91be7f769cab17f54eba.jpg'
+  },
+  {
+    type: 'image',
+    src: '/940f071ce5c16e9f37d0.jpg'
+  },
+  {
+    type: 'image',
+    src: '/992a40bdae60253e7c71.jpg'
+  },
+  {
+    type: 'image',
+    src: '/99e590ed2230a96ef021.jpg'
+  },
+  {
+    type: 'image',
+    src: '/9a12c5de7703fc5da512.jpg'
+  },
+  {
+    type: 'image',
+    src: '/9aa3741298cf13914ade.jpg'
+  },
+  {
+    type: 'image',
+    src: '/a2556ae9d934526a0b25.jpg'
+  },
+  {
+    type: 'image',
+    src: '/a326e7ee07338c6dd522.jpg'
+  },
+  {
+    type: 'image',
+    src: '/a852c7b82765ac3bf574.jpg'
+  },
+  {
+    type: 'image',
+    src: '/a9ee38168bcb009559da.jpg'
+  },
+  {
+    type: 'image',
+    src: '/aef17237c0ea4bb412fb.jpg'
+  },
+  {
+    type: 'image',
+    src: '/aefd9a4a7497ffc9a686.jpg'
+  },
+  {
+    type: 'image',
+    src: '/b30038d5d4085f560619.jpg'
+  },
+  {
+    type: 'image',
+    src: '/b4410ea5bc7837266e69.jpg'
+  },
+  {
+    type: 'image',
+    src: '/b5a6fe0d4dd0c68e9fc1.jpg'
+  },
+  {
+    type: 'image',
+    src: '/b5afd94e3993b2cdeb82.jpg'
+  },
+  {
+    type: 'image',
+    src: '/bb28dc0e3cd3b78deec2.jpg'
+  },
+  {
+    type: 'image',
+    src: '/bcf584f86425ef7bb634.jpg'
+  },
+  {
+    type: 'image',
+    src: '/bd534697f44a7f14265b.jpg'
+  },
+  {
+    type: 'image',
+    src: '/be001dd6ad0b26557f1a.jpg'
+  },
+  {
+    type: 'image',
+    src: '/c006a31240cfcb9192de.jpg'
+  },
+  {
+    type: 'image',
+    src: '/c3a16a7d9aa011fe48b1.jpg'
+  },
+  {
+    type: 'image',
+    src: '/c4afd7b3646eef30b67f.jpg'
+  },
+  {
+    type: 'image',
+    src: '/ca44a8221bff90a1c9ee.jpg'
+  },
+  {
+    type: 'image',
+    src: '/cc6d2edfcb02405c1913.jpg'
+  },
+  {
+    type: 'image',
+    src: '/cfdc6f4edc9357cd0e82.jpg'
+  },
+  {
+    type: 'image',
+    src: '/d344c3bb2366a838f177.jpg'
+  },
+  {
+    type: 'image',
+    src: '/d36dee385de5d6bb8ff4.jpg'
+  },
+  {
+    type: 'image',
+    src: '/d42f14dda7002c5e7511.jpg'
+  },
+  {
+    type: 'image',
+    src: '/d50f8ad57a08f156a819.jpg'
+  },
+  {
+    type: 'image',
+    src: '/d60555e0e53d6e63372c.jpg'
+  },
+  {
+    type: 'image',
+    src: '/d7bb515ea1832add7392.jpg'
+  },
+  {
+    type: 'image',
+    src: '/d9a1dc663ebbb5e5ecaa.jpg'
+  },
+  {
+    type: 'image',
+    src: '/d9aa7fc29f1f14414d0e.jpg'
+  },
+  {
+    type: 'image',
+    src: '/d9d3720582d8098650c9.jpg'
+  },
+  {
+    type: 'image',
+    src: '/db89fca64f7bc4259d6a.jpg'
+  },
+  {
+    type: 'image',
+    src: '/dc87315c838108df5190.jpg'
+  },
+  {
+    type: 'image',
+    src: '/dd698ea36b7ee020b96f.jpg'
+  },
+  {
+    type: 'image',
+    src: '/ddafcd787ea5f5fbacb4.jpg'
+  },
+  {
+    type: 'image',
+    src: '/e0bddf5b2f86a4d8fd97.jpg'
+  },
+  {
+    type: 'image',
+    src: '/e23b4513f7ce7c9025df.jpg'
+  },
+  {
+    type: 'image',
+    src: '/edc2b60e06d38d8dd4c2.jpg'
+  },
+  {
+    type: 'image',
+    src: '/edf13f48da9551cb0884.jpg'
+  },
+  {
+    type: 'image',
+    src: '/ee8aa32e4df3c6ad9fe2.jpg'
+  },
+  {
+    type: 'image',
+    src: '/ef3630de8203095d5012.jpg'
+  },
+  {
+    type: 'image',
+    src: '/ef9ce4ea07378c69d526.jpg'
+  },
+  {
+    type: 'image',
+    src: '/f0ef353f85e20ebc57f3.jpg'
+  },
+  {
+    type: 'image',
+    src: '/f0efe2a0507ddb23826c.jpg'
+  },
+  {
+    type: 'image',
+    src: '/f37151e4e33968673128.jpg'
+  },
+  {
+    type: 'image',
+    src: '/f7fdbb980845831bda54.jpg'
+  },
+  {
+    type: 'image',
+    src: '/f84a0042b39f38c1618e.jpg'
+  },
+  {
+    type: 'image',
+    src: '/ffd104a6b77b3c25656a.jpg'
+  },
+  {
+    type: 'image',
+    src: '/IMG_20250618_083707.jpg'
+  },
+  {
+    type: 'image',
+    src: '/Messenger_creation_1535E073-CBFF-4944-8E22-12593BB730F0.jpeg'
+  },
+  {
+    type: 'image',
+    src: '/Messenger_creation_18E7AAF5-A9C5-4E4D-81B5-65E06746B19A.jpeg'
   },
   {
     type: 'video',
-    src: '../media/Messenger_creation_E5E09AC5-AC9F-4B62-97A5-55ED86FBF6B3.mp4'
+    src: '/Messenger_creation_1F24CF12-9F3B-492C-ACDD-9629324A441D_1.mp4'
+  },
+  {
+    type: 'image',
+    src: '/Messenger_creation_2B7ED779-8884-491C-B233-33947A2BA399.jpeg'
+  },
+  {
+    type: 'image',
+    src: '/Messenger_creation_40D80B68-2B2D-4101-B440-052BC955AED9.jpeg'
+  },
+  {
+    type: 'image',
+    src: '/Messenger_creation_60C2D54F-D6A8-4638-887C-69BAED18AD35_1.jpeg'
+  },
+  {
+    type: 'image',
+    src: '/Messenger_creation_6F3ED426-7196-44F7-9C76-0D1A1DB764BF_1.jpeg'
   },
   {
     type: 'video',
-    src: '../media/Messenger_creation_ED515887-E567-4735-8DEE-87D77D7FD7C9.mp4'
+    src: '/Messenger_creation_B88301E6-8F0E-46DD-9B47-B1107D53084A.mp4'
+  },
+  {
+    type: 'video',
+    src: '/Messenger_creation_E5E09AC5-AC9F-4B62-97A5-55ED86FBF6B3.mp4'
+  },
+  {
+    type: 'video',
+    src: '/Messenger_creation_ED515887-E567-4735-8DEE-87D77D7FD7C9.mp4'
   }
 ];
